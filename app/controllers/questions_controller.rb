a98652b67070a31d83a689c13165d51f388d9488
zijingkek <chrisartbp@gmail.com>
2020-06-22 12:21:16 +0800

    fisnih

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:answer]
+    @answer = "I don't care, get dressed and go to work!"
+    if params[:answer] == "I am going to work"
+      @answer = " Great!"
+    elsif params[:answer].include? "?"
+      @answer =  "Silly question, get dressed and go to work!."
+
+    end
+  end
+end
